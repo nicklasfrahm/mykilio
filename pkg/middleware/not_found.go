c61613cdc301ec7698f4e341c0fd4dc2b5a09143
nicklasfrahm <nicklas.frahm@gmail.com>
2021-05-21 23:29:07 +0200

    Log failed requests

--- pkg/middleware/not_found.go
+++ pkg/middleware/not_found.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog/log"
 
 	"github.com/nicklasfrahm/mykilio/pkg/util"
 )
@@ -9,6 +10,7 @@ import (
 // NotFound returns a middlware for endpoints that do not exist.
 func NotFound() func(*fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
+		log.Warn().Msgf("404 %s", string(c.Request().RequestURI()))
 		return util.SemanticError(c, &util.ReasonEndpointUnsupported)
 	}
 }
